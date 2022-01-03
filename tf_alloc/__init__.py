4620d6b146973f0f40404d236cc97783045d759d
고준서 <sta06167@naver.com>
2022-01-03 19:21:44 +0900

    🧩 Update codes

--- tf_alloc/__init__.py
+++ tf_alloc/__init__.py
@@ -1,9 +1,15 @@
+# __init__.py
+# Copyright (C) 2022 (sta06167@naver.com) and contributors
+# Junseo Ko
+# github.com/korkite
+
 try:
     import tensorflow as tf
 
 except:
     raise ModuleNotFoundError("You must install tensorflow for your environment first. This package doesn't contain tensorflow installation.")
 
+__version__ = "0.0.1"
 
 from .gpu import get_gpu_objects
 from .allocate import allocate
